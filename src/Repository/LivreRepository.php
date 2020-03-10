71d5a738509095c373bec90dc14ed6b693bf0d35
martasevo <marta.sevo@gmail.com>
2020-03-10 09:55:18 +0100

    Premier commit

---
+++ src/Repository/LivreRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Livre;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method Livre|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Livre|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Livre[]    findAll()
+ * @method Livre[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class LivreRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Livre::class);
+    }
+
+     /**
+      * @return Livre[] Returns an array of Livre objects
+      */
+    
+    public function findByAuteur($value)
+    {
+        //SELECT * FROM livre l WHERE l.auteur LIKE "%" . $value . "%" 
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.auteur LIKE :val')
+            ->setParameter('val', "%" . $value . "%")
+            ->orderBy('l.titre', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    // /**
+    //  * @return Livre[] Returns an array of Livre objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('l.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Livre
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
